21247f8156169acb8001873a0b12cef3a5c2ef90
theironcook <bart.wood@henryschein.com>
2018-11-28 13:21:46 -0700

    'hi

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.port || 3000;
+const port = process.env.port || 2500;
 
 const app = express();
 app.use(express.static(publicPath));
