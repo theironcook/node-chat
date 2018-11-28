afbfd9fa0accf7e1839e80533384f2eea7edf25b
theironcook <bart.wood@henryschein.com>
2018-11-28 13:37:13 -0700

    'stuff'

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log(`new user connected`); 
+
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
+  });
+});
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-})
-
+server.listen(port, () => {
+  console.log(`listening on ${port}`); 
+});
 
