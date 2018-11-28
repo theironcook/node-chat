2cb5ee954e09246401ddf31760450aa47353fad5
Bart Wood <bart.wood@henryschein.com>
2018-11-28 13:00:33 -0700

    'hi'

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.port || 3000;
+
+const app = express();
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+})
+
+
